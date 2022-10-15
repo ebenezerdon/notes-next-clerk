9ec8adf6032f6d4fa1a57999aab7fe1301fe9cb4
ebenezerdon <ebenezerdonu@gmail.com>
2022-10-15 04:27:12 +0100

    set up clerk

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import { useRouter } from 'next/router'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { push } = useRouter()
+
+  return (
+    <ClerkProvider frontendApi="clerk.open.bear-94.lcl.dev" navigate={(to) => push(to)}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  )
 }
 
 export default MyApp
